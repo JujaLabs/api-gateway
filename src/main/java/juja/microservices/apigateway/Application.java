cccf17adeae0abadd92207994c8087166342ddf9
Ivan Shapovalov <ivshapovalov@gmail.com>
2017-10-09 13:47:03 +0300

    #1 - add base dependencies for empty zuul project

--- src/main/java/juja/microservices/apigateway/Application.java
+++ src/main/java/juja/microservices/apigateway/Application.java
@@ -2,12 +2,14 @@ package juja.microservices.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author Danil Kuznetsov (kuznetsov.danil.v@gmail.com)
+ * @author Ivan Shapovalov
  */
-
 @SpringBootApplication
+@EnableZuulProxy
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
